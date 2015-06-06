45fcbb7d9bbdeacb76a3510900221d739b280969
Sarah <sarah.hanna@gmail.com>
2015-06-05 22:40:54 -0700

    coating data by cluster

--- WHI_long_term_coating_histos_by_air_mass.py
+++ WHI_long_term_coating_histos_by_air_mass.py
@@ -10,12 +10,12 @@ VED_max = 180
 
 
 os.chdir('C:/Users/Sarah Hanna/Documents/Data/WHI long term record/coatings/')
-file = open('coating thicknesses by air mass for 69.76nm to 220.11nm.binpickl', 'r')
+file = open('coating thicknesses by air mass for 69.76nm to 220.11nm-spikes_fixed-2hr_clusters.binpickl', 'r')
 coating_data = pickle.load(file)
 file.close()
 
 coating_info = {}
-
+lists = []
 for air_mass_name, air_mass_info in coating_data.iteritems():
 	print air_mass_name
 	coatings=[]
@@ -26,66 +26,85 @@ for air_mass_name, air_mass_info in coating_data.iteritems():
 		
 		
 		if rBC_VED >= VED_min and rBC_VED < VED_max:
-			coatings.append(Dshell_Dcore)
-	
-	coating_info[air_mass_name] = coatings
-	
+			if air_mass_name in ['cluster_1']:
+				air_mass = 'Bering'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+					
+			if air_mass_name in ['cluster_2']:
+				air_mass = 'N Coastal/Continental'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+					
+			if air_mass_name in ['cluster_3']:
+				air_mass = 'N Pacific'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+					
+			if air_mass_name in ['cluster_4','cluster_6']:
+				air_mass = 'S Pacific'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+					
+			if air_mass_name in ['cluster_5']:
+				air_mass = 'W Pacific/Asia'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+
+			if air_mass_name in ['cluster_GBPS']:
+				air_mass = 'GBPS'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+					
+			if air_mass_name in ['fresh']:
+				air_mass = 'Fresh Emissions'
+				if air_mass in coating_info:
+					coating_info[air_mass].append(coat_th)
+				else:
+					coating_info[air_mass] = [coat_th]
+
+		
 #####plotting
 
+for key,data in coating_info.iteritems():
+	air_mass = key
+	air_mass_coating_info = data
+	print air_mass, np.median(air_mass_coating_info), np.mean(air_mass_coating_info), 
+	print '\n'
 
-fig = plt.figure(figsize=(6,12))
+	
+fig, axes = plt.subplots(7,1, figsize=(6, 12), facecolor='w', edgecolor='k')
+fig.subplots_adjust(hspace = 0., wspace=0.08)
+
+axs = axes.ravel()
+
 bin_number = 20
+y_ulim = 0.01
 
-ax1 =  plt.subplot2grid((6,1), (0,0), )
-ax2 =  plt.subplot2grid((6,1), (1,0),sharex=ax1)
-ax3 =  plt.subplot2grid((6,1), (2,0),sharex=ax1)
-ax4 =  plt.subplot2grid((6,1), (3,0),sharex=ax1)
-ax5 =  plt.subplot2grid((6,1), (4,0),sharex=ax1)
-ax6 =  plt.subplot2grid((6,1), (5,0),sharex=ax1)
+i = 0
+for key,data in coating_info.iteritems():
+	axs[i].hist(coating_info[key], bins=bin_number,histtype='step', normed = 1)
+	axs[i].text(0.6, 0.8,key, transform=axs[i].transAxes)
+	#axs[i].set_ylim(0,y_ulim)
+	axs[i].set_xlim(-40,100)
+	#axs[i].yaxis.set_visible(False)
+	axs[i].axvline(np.median(data), color='r', linestyle='--')
 
-ax1.hist(coating_info['GBPS'], bins=bin_number,histtype='step', label = 'GBPS', normed = 1)
-ax1.text(0.6, 0.8,'GBPS', transform=ax1.transAxes)
-ax1.yaxis.set_visible(False)
-ax1.axvline(np.median(coating_info['GBPS']), color='r', linestyle='--')
-#ax1.set_xlim(-80,178)
-ax1.set_xlim(0,3)
-ax1.set_title(str(VED_min) + '-' + str(VED_max) + 'nm rBC cores')
+	i+=1
 
-ax2.hist(coating_info['Cont'], bins=bin_number,histtype='step', label = 'N Can', normed = 1)
-ax2.text(0.6, 0.8,'N Can', transform=ax2.transAxes)
-ax2.yaxis.set_visible(False)
-ax2.axvline(np.median(coating_info['Cont']), color='r', linestyle='--')
-
-
-ax3.hist(coating_info['LRT'], bins=bin_number,histtype='step', label = 'W Pac/Asia', normed = 1)
-ax3.text(0.6, 0.8,'W Pac/Asia', transform=ax3.transAxes)
-ax3.yaxis.set_visible(False)
-ax3.axvline(np.median(coating_info['LRT']), color='r', linestyle='--')
-
-ax4.hist(coating_info['SPac'], bins=bin_number,histtype='step', label = 'S Pac', normed = 1)
-ax4.text(0.6, 0.8,'S Pac', transform=ax4.transAxes)
-ax4.yaxis.set_visible(False)
-ax4.axvline(np.median(coating_info['SPac']), color='r', linestyle='--')
-
-
-ax5.hist(coating_info['NPac'], bins=bin_number,histtype='step', label = 'N Pac', normed = 1)
-ax5.text(0.6, 0.8,'N Pac', transform=ax5.transAxes)
-ax5.yaxis.set_visible(False)
-ax5.axvline(np.median(coating_info['NPac']), color='r', linestyle='--')
-ax5.set_xlabel('Coating Thickness (nm)')
-
-
-ax6.hist(coating_info['fresh'], bins=bin_number,histtype='step', label = 'fresh rBC', normed = 1)
-ax6.text(0.6, 0.8,'fresh rBC', transform=ax6.transAxes)
-ax6.yaxis.set_visible(False)
-ax6.axvline(np.median(coating_info['fresh']), color='r', linestyle='--')
-#ax6.set_xlabel('Coating Thickness (nm)')
-ax6.set_xlabel('Dshell/Dcore')
-
-plt.subplots_adjust(hspace=0.0)
-plt.subplots_adjust(wspace=0.0)
-
-os.chdir('C:/Users/Sarah Hanna/Documents/Data/WHI long term record/coatings/')
-plt.savefig('coating (Dshell_Dcore) histos by air mass' + str(VED_min) + '-' +str(VED_max) + 'nm rBC cores.png', bbox_inches='tight')
+#os.chdir('C:/Users/Sarah Hanna/Documents/Data/WHI long term record/coatings/')
+#plt.savefig('coating (Dshell_Dcore) histos by air mass' + str(VED_min) + '-' +str(VED_max) + 'nm rBC cores-v2.png', bbox_inches='tight')
 
 plt.show()      
